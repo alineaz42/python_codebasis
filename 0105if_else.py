87da7f30f9ff6d2826dcb72f21f2f04b84835920
Ali Neaz <neazali77@gmail.com>
2021-12-10 02:16:30 +0600

    if else statement is not completed but started

---
+++ 0105if_else.py
@@ -0,0 +1,10 @@
+# take a number from the user and print the number if even or odd
+
+number = int(input("Enter a number: "))
+if number % 2 == 0 and number > 20:
+    print("Ther number is even")
+    print("The number is greater than 20")
+else:
+    print("Ther number is odd.")
+    print("The number is not greater than 2020")
+print("Hello Coding")
---
+++ all.py
@@ -0,0 +1,10 @@
+# sum = 25+45
+# strdiv = 45/15
+# multiplication = 41*2
+# remainder = 13%3
+# print(remainder)
+
+print((3+3)*2)
+num1 = 13
+num2 = "45"
+print(type(num1),type(num2))
