84599ec16f0af810ec11bb928fce882063c5a07e
Ali Neaz <neazali77@gmail.com>
2021-12-10 02:27:21 +0600

    done with the if else elif statement

--- 0105if_else.py
+++ 0105if_else.py
@@ -1,10 +1,24 @@
 # take a number from the user and print the number if even or odd
 
-number = int(input("Enter a number: "))
-if number % 2 == 0 and number > 20:
-    print("Ther number is even")
-    print("The number is greater than 20")
+# number = int(input("Enter a number: "))
+# if number % 2 == 0 and number > 20:
+#     print("Ther number is even")
+#     print("The number is greater than 20")
+# else:
+#     print("Ther number is odd.")
+#     print("The number is not greater than 2020")
+# print("Hello Coding")
+indian = ["samosa", "daal", "naan"]
+chinese = ["egg role", "pot sticker", "fried rice"]
+italian = ["pizza", "pasta", "ristto"]
+
+dish = input("Enter a dish name: ")
+if dish in indian:
+    print("The dish is indian")
+elif dish in chinese:
+    print("The dish is chinese")
+elif dish in italian:
+    print("The dish is italian")
 else:
-    print("Ther number is odd.")
-    print("The number is not greater than 2020")
-print("Hello Coding")
+    print("The dish is not in the menu")
+    print("We are sorry sir!!")
